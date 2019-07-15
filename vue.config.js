f413757b4aa6218a3931c81d1018457e06a48b97
shiyunhao <532412829@qq.com>
2019-07-15 16:46:38 +0800

    config文件更改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // 当 vue 的环境处于 puoduction（生产环境）时，将 publicPath 改成 /1903-vue-shequ/  ,开发环境时  /
-  publicPath: process.env.NODE_ENV === "production" ? "/1903-vue-shequ/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-shequ/" : "/"
 };
